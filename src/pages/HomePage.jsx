0117f1a863aa0c1ca06356c8bef0c61b4b27fa21
domspenc <dommspencer@gmail.com>
2024-11-19 18:06:18 +0800

    added project cards and css files for projects and homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
+import { allProjects } from "../data";
+import ProjectCard from "../components/ProjectCard";
+
 function HomePage() {
-    return <h1>This is the home page.</h1>;
-  }
-  
-  export default HomePage;
+  return (
+        <div>
+            {allProjects.map((projectData, key) => {
+              return <ProjectCard key={key} projectData={projectData} />;
+            })}
+        </div>
+    );
+}
+
+export default HomePage
